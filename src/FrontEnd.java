ea6dee1643d63452a8f530741a3c2b0216e31bc3
duko栖泽 <dukoqize@dukoqizemacbook.local>
2017-10-17 10:40:11 -0400

    ..

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -11,11 +11,6 @@ public class FrontEnd {
 		
 	}
 	
-	public static void readFile2() {
-		
-	}
-	
-	
 	// write the transaction summary file
 	public static void writeFile(String input) {
 		
@@ -26,6 +21,16 @@ public class FrontEnd {
 		Scanner screen = new Scanner(System.in);
 		String input = screen.nextLine();
 		return input;
+	}
+	
+	// check if the input is good.
+	public static boolean checkInputOK(String input; String[] validInput) {
+		boolean inputOk = false;
+		for (valid : validInput) {
+			if (input.toLowerCase().equals(valid.toLowerCase()))
+				inputOK = true;
+		}
+		reutrn inputOK;
 	}
 	
 	
@@ -42,7 +47,14 @@ public class FrontEnd {
 	}
 	
 	public static void deposit() {
-		
+		do {
+			System.out.println("Please enter account number: ");
+			String acc = getInput();
+		} while (checkInputOK(input, getAllAccountNumber()));
+		do { 
+			System.out.println("Please enter the amount to deposit in cents: ");
+			String amountString = getInput();
+		} while ()
 	}
 	
 	public static void deleteacc() {
@@ -53,22 +65,68 @@ public class FrontEnd {
 		
 	}
 	
+	// show the prompt when the user is an agent an get the transaction
+	public static String loginAgent() {
+		System.out.println("Please select a transaction");
+		System.out.println("A. creating a new account");
+		System.out.println("B. delete an existing account");
+		System.out.println("C. deposit to an account");
+		System.out.println("D. withdraw from an account");
+		System.out.println("E. transfer from one account to another");
+		do {
+			System.out.println("Please enter the letter: ");
+			String input = getInput();
+		} while (checkInputOK(input, ["A", "B", "C", "D", "E"]));
+		return input.toLowerCase();
+	}
+	
+	public static String loginATM() {
+		System.out.println("Please select a transaction and enter the letter: ");
+		System.out.println("A. deposit to an account");
+		System.out.println("B. withdraw from an account");
+		System.out.println("C. transfer from one account to another");
+		do {
+			System.out.println("Please enter the letter: ");
+			String input = getInput();
+		} while (checkInputOK(input, ["A", "B", "C"]));
+		return input.toLowerCase();
+	}
+	
 	// get the user, get the transaction and call the other transaction
 	public static void login() {
-		
+		do {
+			System.out.println("Login as: \"ATM\" or \"agent\")");
+			user = getInput();
+		} while (checkInputOK(user, ["ATM", "agent"]));
+		if (user.equals("agent")) 
+			String tran = loginAgent();
+		else
+			String tran = loginATM();
+		if (tran.equals("a"))
+			creatacc();
+		else if (tran.equals("b"))
+			deleteacc();
+		else if (tran.equals("c"))
+			deposit();
+		else if (tran.equals("d"))
+			withdraw();
+		else
+			transfer;
 	}
 	
 	// show the welcome message, read the account file 
 	// get the login input and call the login method.
 	public static void main(String[] args) {
-		System.out.println("Welcome to QBASIC");
-		boolean inputOK = false;
-		do {
-			System.out.println("please type login");
-			String input = getInput();
-			if (input.equals("login"))
-				inputOK = true;
-		} while (!inputOK);
-		login();
+		while (true) {
+			System.out.println("Welcome to QBASIC");
+			boolean inputOK = false;
+			do {
+				System.out.println("please type login");
+				String input = getInput();
+				if (input.equals("login"))
+					inputOK = true;
+			} while (!inputOK);
+			login();
+		}
 	}
 }
