412853f7abdd7458a0077ed326b8baa1d1d01cfe
alexandrabratton <alex.bratton@queensu.ca>
2017-10-18 22:22:01 -0400

    fixing up the write file

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -108,7 +108,6 @@ public class FrontEnd {
 	 * @return a String array containing all the account numbers from the account file.
 	 */
 	private static String[] getAllAccNumStr() {
-        // Using the ArrayList<Account> accounts
 		ArrayList<Integer> accountNumbersFromAccount = new ArrayList<>();
 		for (Account account : accounts) {
 			accountNumbersFromAccount.add(account.getAccountNumber());
@@ -128,6 +127,11 @@ public class FrontEnd {
 	private static void initializeLogFile() {
 	    try {
 	        logFile = new PrintWriter("transaction-log.txt", "UTF-8");
+	        if (user != null) {
+                writeFile("Session initiated with user type: " + user.getName());
+            } else {
+	            writeFile("Session initiated.");
+            }
         } catch (IOException e) {
             System.out.println("Error creating log file.");
             System.exit(1);
@@ -138,6 +142,7 @@ public class FrontEnd {
      * Called when the program is closed, to close the log file.
      */
     private static void closeLogFile() {
+        writeFile("Session closed.");
         logFile.close();
     }
 
@@ -398,7 +403,7 @@ public class FrontEnd {
 			userInput = getInput().toLowerCase();
 			if (userInput.equalsIgnoreCase("agent"))
 				user = UserType.AGENT;
-			else if (userInput.equalsIgnoreCase("atm"))
+			else if (userInput.equalsIgnoreCase("machine"))
 				user = UserType.ATM;
 		} while (!checkInputOK(userInput, validInput));
 
